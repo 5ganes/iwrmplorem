f32657baed2f379472c187fe841438956c822d46
ganesh khatri <kh6ganesh@gmail.com>
2018-03-28 15:58:27 +0545

    logo title changed

--- data/constants.php
+++ data/constants.php
@@ -5,7 +5,7 @@ define("ADMIN_LEFT_WIDTH", "20%");
 define("ADMIN_BODY_WIDTH", "80%");
 define("ADMIN_TITLE", "Admin Control Panel");
 define("PAGE_TITLE", "Irrigation and Water Resource Management Project");
-define("SITE_URL", "http://iwrm.gov.np/");
+define("SITE_URL", "http://localhost/iwrm/");
 
 
 ////////////////IMAGE FOLDER LOCATIONS////////////////////////
